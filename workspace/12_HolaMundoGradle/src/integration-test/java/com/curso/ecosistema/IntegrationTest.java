1caa4106d62f6f5f5af3be7142ef653b8b0863d6
Victor Herrero Cazurro <victorherrerocazurro@gmail.com>
2018-06-14 19:29:11 +0200

    Dia 4

---
+++ workspace/12_HolaMundoGradle/src/integration-test/java/com/curso/ecosistema/IntegrationTest.java
@@ -0,0 +1,14 @@
+package com.curso.ecosistema;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntegrationTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
